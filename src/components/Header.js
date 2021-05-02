c71eb86fff10a1e97ae80c36a8733022fbbc9176
Byron Rivera <byronskb@gmail.com>
2021-05-02 00:11:11 -0600

    Add form functionallity and toggle reminder

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,24 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click')
-    }
-
+const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button 
+                color={showAdd ? 'red' : 'green'} 
+                text={showAdd ? 'Close' : 'Add Task'} 
+                onClick={onAdd}/>
         </header>
     )
 }
 
-// Setting up default properties for a component
+// Setting up default properties for a component if they are not passed in the function
 Header.defaultProps = {
     title: 'Task Tracker',
 }
 
-// Setting types for properties to validate they are correct
+// Setting types for properties to validate their type and options
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
