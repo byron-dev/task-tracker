689cc48a89d883ae64f47950671c98600fca1020
Byron Rivera <byronskb@gmail.com>
2021-05-03 13:17:46 -0600

    Fetch and delete data from json db

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import AddTask from './components/AddTask'
 
 function App() {
     const [showAddTask, setShowAddTask] = useState(false)
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at school',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false,
-        },
-    ])
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        const getTasks = async () => {
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
+        }
+        getTasks()
+    }, [])
+
+    const fetchTasks = async () => {
+        const res = await fetch('http://localhost:5000/tasks')
+        const data  = await res.json()
+        return data
+    }
 
     const addTask = (task) => {
         const id = Math.floor(Math.random() * 10000) + 1
@@ -32,7 +27,11 @@ function App() {
         setTasks([...tasks, newTask])
     }
 
-    const deleteTask = (id) => {
+    const deleteTask = async (id) => {
+        await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'DELETE'
+        })
+
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
